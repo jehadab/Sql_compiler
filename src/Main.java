297f67c902c98d7d9a6ec4d0211b089751ddbc59
Jehadab <jehad1996@hotmail.com>
2019-11-15 17:00:57 +0200

    just after main merge between three projects

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i = 0  ;
+        boolean bol = false;
+        i = i = i+1 ;        //todo i = i = i+1 check
+      //  i = (j++) + j + 1 // todo  i = (j++) + j + 1
+        if(  true != i >= 1  ){}
+
+        v();
+        while (true == 5 >2 );
+
+
+
+    }
+    public static void v (){
+
+    }
+}
